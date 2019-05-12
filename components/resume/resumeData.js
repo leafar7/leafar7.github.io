6e8fccb6eddeef749a0c3a51e96ad97e9e63846d
leafar7 <rafelvieira77@hotmail.com>
2019-05-12 00:33:04 -0300

    initial commit

---
+++ components/resume/resumeData.js
@@ -0,0 +1,18 @@
+
+
+let resumeData = {
+   // "imagebaseurl":"https://github.com/leafar7/leafar7.github.io",
+    "name": "Rafael Vieira Cano",
+    "role": "React Developer",
+    "roleDescription": "Currently learning about Reactjs and all the ecosystems aligned to this technology"
+    }    
+
+
+
+
+    
+  export default resumeData;
+
+
+
+
