36b2bffd3558bea8ea80c0a34d154cd4ba4880f9
leafar7 <rafelvieira77@hotmail.com>
2019-05-13 09:33:08 -0300

    Updates

---
+++ precache-manifest.fc422233ccd74c1a37f8a8d018ef9d30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "daec6b25146006f3baf4",
+    "url": "/me/static/js/main.cc6f7665.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/me/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "daec6b25146006f3baf4",
+    "url": "/me/static/css/main.89495659.chunk.css"
+  },
+  {
+    "revision": "edeb76f7eb00757c618b5b7b2990eb3e",
+    "url": "/me/index.html"
+  }
+];
