e33725eeaebe85c7d9dc518f5f2c5dc269972a7f
leafar7 <rafelvieira77@hotmail.com>
2021-06-18 23:59:39 -0300

    teste mudança código abertura

--- src/components/resume/resumeData.js
+++ src/components/resume/resumeData.js
@@ -4,7 +4,7 @@ let resumeData = {
    // "imagebaseurl":"https://github.com/leafar7/leafar7.github.io",
     "name": "Projeto Aconchego",
     "role": "Levar aconchego ",
-    "roleDescription": "Cao coração dos necessitados"
+    "roleDescription": "ao coração de quem precisa."
     }    
 
 
