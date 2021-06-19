5bbbafb2a4faa0de757e8efd4ba8148f77f61eb5
leafar7 <rafelvieira77@hotmail.com>
2021-06-19 00:22:30 -0300

    trial pull one

--- src/components/resume/resumeData.js
+++ src/components/resume/resumeData.js
@@ -3,8 +3,8 @@
 let resumeData = {
    // "imagebaseurl":"https://github.com/leafar7/projeto-aconchego",
     "name": "Projeto Aconchego",
-    "role": "Levar aconchego ",
-    "roleDescription": "ao coração de quem precisa."
+    "role": "Levar aconchego",
+    "roleDescription": " ao coração de quem precisa."
     }    
 
 
